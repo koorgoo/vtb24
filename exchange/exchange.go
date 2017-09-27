88a0ed386674832cc62d6d5d05cd3a2f3d8b71d9
Dima Kurguzov <koorgoo@gmail.com>
2017-09-28 00:48:40 +0300

    Add exchange with thresholds

--- exchange/exchange.go
+++ exchange/exchange.go
@@ -1,20 +1,89 @@
 package exchange
 
+import (
+	"errors"
+	"sort"
+)
+
+var (
+	ErrNegativeAmount = errors.New("exchange: negative amount")
+	ErrNoRate         = errors.New("exchange: no rate")
+)
+
+type Func func(float64) (float64, error)
+
 type Interface interface {
-	Buy(float64) float64
-	Sell(float64) float64
+	Buy(float64) (float64, error)
+	Sell(float64) (float64, error)
 }
 
 type Rate struct {
 	Buy  float64
 	Sell float64
+
+	// Threshold sets the minimum amount for exchange. Threshold is used only
+	// in exchanger returned by NewWithThresholds().
+	Threshold float64
 }
 
 func New(rate Rate) Interface {
-	return &basic{Rate: rate}
+	return &basicEx{Rate: &rate}
 }
 
-type basic struct{ Rate Rate }
+type basicEx struct{ Rate *Rate }
 
-func (b *basic) Buy(x float64) float64  { return b.Rate.Buy * x }
-func (b *basic) Sell(x float64) float64 { return b.Rate.Sell * x }
+func (e *basicEx) Buy(x float64) (float64, error)  { return exchange(x, e.Rate.Buy) }
+func (e *basicEx) Sell(x float64) (float64, error) { return exchange(x, e.Rate.Sell) }
+
+func exchange(x, rate float64) (y float64, err error) {
+	if x < 0 {
+		err = ErrNegativeAmount
+	} else {
+		y = x * rate
+	}
+	return
+}
+
+func NewWithThresholds(rates ...Rate) Interface {
+	e := new(withThresholdsEx)
+	for i := range rates {
+		e.entries = append(e.entries, &basicEx{Rate: &rates[i]})
+	}
+	e.sortEntries()
+	return e
+}
+
+type withThresholdsEx struct {
+	entries []*basicEx
+}
+
+func (e *withThresholdsEx) sortEntries() {
+	sort.Slice(e.entries, func(i, j int) bool {
+		return e.entries[i].Rate.Threshold > e.entries[j].Rate.Threshold
+	})
+}
+
+func (e *withThresholdsEx) Buy(x float64) (float64, error) {
+	return e.exchange(x, func(e Interface) Func { return e.Buy })
+}
+
+func (e *withThresholdsEx) Sell(x float64) (float64, error) {
+	return e.exchange(x, func(e Interface) Func { return e.Sell })
+}
+
+type xChoose func(Interface) Func
+
+func (e *withThresholdsEx) exchange(x float64, choose xChoose) (y float64, err error) {
+	for _, ex := range e.entries {
+		if ex.Rate.Threshold > x {
+			continue
+		}
+		y, err = choose(ex)(x)
+		return
+	}
+	_, err = exchange(x, 1)
+	if err == nil {
+		err = ErrNoRate
+	}
+	return
+}
--- exchange/exchange_test.go
+++ exchange/exchange_test.go
@@ -5,7 +5,10 @@ import (
 	"testing"
 )
 
-type Table map[float64]float64
+type Table map[float64]struct {
+	Amount float64
+	Err    error
+}
 
 var BasicTests = []struct {
 	Rate Rate
@@ -14,26 +17,85 @@ var BasicTests = []struct {
 }{
 	{
 		Rate: Rate{Buy: 2, Sell: 3},
-		Buy:  Table{1: 2, 2.5: 5},
-		Sell: Table{1: 3, 2.5: 7.5},
+		Buy: Table{
+			-1:  {0, ErrNegativeAmount},
+			0:   {0, nil},
+			1:   {2, nil},
+			2.5: {5, nil},
+		},
+		Sell: Table{
+			-1:  {0, ErrNegativeAmount},
+			0:   {0, nil},
+			1:   {3, nil},
+			2.5: {7.5, nil},
+		},
 	},
 }
 
 func TestNew(t *testing.T) {
 	for i := range BasicTests {
 		tt := BasicTests[i]
+		e := New(tt.Rate)
 		t.Run(fmt.Sprintf("%+v", tt.Rate), func(t *testing.T) {
-			e := New(tt.Rate)
-			for a, b := range tt.Buy {
-				if c := e.Buy(a); c != b {
-					t.Errorf("buy %v: want %v, got %v", a, b, c)
-				}
-			}
-			for a, b := range tt.Sell {
-				if c := e.Sell(a); c != b {
-					t.Errorf("sell %v: want %v, got %v", a, b, c)
-				}
-			}
+			test(t, e, tt.Buy, tt.Sell)
 		})
+	}
+}
+
+var ThresholdsTests = []struct {
+	Rates []Rate
+	Buy   Table
+	Sell  Table
+}{
+	{
+		Rates: []Rate{
+			{Buy: 2, Sell: 3, Threshold: 10},
+			{Buy: 3, Sell: 6, Threshold: 20},
+		},
+		Buy: Table{
+			-1:  {0, ErrNegativeAmount},
+			0:   {0, ErrNoRate},
+			1:   {0, ErrNoRate},
+			10:  {20, nil},
+			15:  {30, nil},
+			20:  {60, nil},
+			100: {300, nil},
+		},
+		Sell: Table{
+			-1:  {0, ErrNegativeAmount},
+			0:   {0, ErrNoRate},
+			1:   {0, ErrNoRate},
+			10:  {30, nil},
+			15:  {45, nil},
+			20:  {120, nil},
+			100: {600, nil},
+		},
+	},
+}
+
+func TestNewWithThresholds(t *testing.T) {
+	for i := range ThresholdsTests {
+		tt := ThresholdsTests[i]
+		e := NewWithThresholds(tt.Rates...)
+		t.Run(fmt.Sprintf("%+v", tt.Rates), func(t *testing.T) {
+			test(t, e, tt.Buy, tt.Sell)
+		})
+	}
+}
+
+func test(t *testing.T, e Interface, Buy, Sell Table) {
+	t.Run("buy", func(t *testing.T) { testFunc(t, e.Buy, Buy) })
+	t.Run("sell", func(t *testing.T) { testFunc(t, e.Sell, Sell) })
+}
+
+func testFunc(t *testing.T, exchange Func, table Table) {
+	for amount, r := range table {
+		n, err := exchange(amount)
+		if n != r.Amount {
+			t.Errorf("%v: want %v, got %v", amount, r.Amount, n)
+		}
+		if err != r.Err {
+			t.Errorf("%v: error: want %q, got %q", amount, r.Err, err)
+		}
 	}
 }
