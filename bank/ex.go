2f759b621548aca07ac9a61a2eec0feb82a8adb5
Dima Kurguzov <koorgoo@gmail.com>
2017-09-30 03:33:38 +0300

    Transform Ex to interface

--- bank/ex.go
+++ bank/ex.go
@@ -7,22 +7,32 @@ import (
 	"github.com/koorgoo/vtb24/exchange"
 )
 
-type Ex struct {
-	// Src is a currency to exchange (buy or sell).
-	Src string
-	// Dst is a currency to exchange to (sold or bought).
-	Dst string
-	// Group is a currency group affecting rates.
-	Group string
+type Ex interface {
+	// Src returns a currency to exchange (buy or sell).
+	Src() string
+	// Dst returns a currency to exchange to (sold or bought).
+	Dst() string
+	// Group returns a currency group affecting rates.
+	Group() string
+
 	exchange.Interface
 }
 
-func (e *Ex) String() string {
-	group := api.GroupText(e.Group)
-	return fmt.Sprintf("%s › %s (%s)", e.Src, e.Dst, group)
+type ex struct {
+	src, dst, group string
+	exchange.Interface
 }
 
-func ParseEx(resp *api.Response) []*Ex {
+func (e *ex) Src() string   { return e.src }
+func (e *ex) Dst() string   { return e.dst }
+func (e *ex) Group() string { return e.group }
+
+func (e *ex) String() string {
+	group := api.GroupText(e.group)
+	return fmt.Sprintf("%s › %s (%s)", e.src, e.dst, group)
+}
+
+func ParseEx(resp *api.Response) []Ex {
 	// Group rates by src, dst, and group.
 	m := map[string]map[string]map[string][]exchange.Rate{}
 	for _, item := range resp.Items {
@@ -44,12 +54,12 @@ func ParseEx(resp *api.Response) []*Ex {
 		})
 	}
 
-	var v []*Ex
+	var v []Ex
 	for src := range m {
 		for dst := range m[src] {
 			for group, rates := range m[src][dst] {
 				e := exchange.New(rates...)
-				ex := &Ex{Src: src, Dst: dst, Group: group, Interface: e}
+				ex := &ex{src: src, dst: dst, group: group, Interface: e}
 				v = append(v, ex)
 			}
 		}
@@ -57,8 +67,8 @@ func ParseEx(resp *api.Response) []*Ex {
 	return v
 }
 
-func FilterEx(v []*Ex, filters ...ExFilter) []*Ex {
-	var a []*Ex
+func FilterEx(v []Ex, filters ...ExFilter) []Ex {
+	var a []Ex
 	for _, ex := range v {
 		if filterEx(ex, filters) {
 			a = append(a, ex)
@@ -67,7 +77,7 @@ func FilterEx(v []*Ex, filters ...ExFilter) []*Ex {
 	return a
 }
 
-func filterEx(e *Ex, filters []ExFilter) bool {
+func filterEx(e Ex, filters []ExFilter) bool {
 	for _, filter := range filters {
 		if !filter(e) {
 			return false
@@ -76,15 +86,15 @@ func filterEx(e *Ex, filters []ExFilter) bool {
 	return true
 }
 
-type ExFilter func(*Ex) bool
+type ExFilter func(Ex) bool
 
 func WithSrcDst(srcdst ...string) ExFilter {
 	if len(srcdst)%2 == 1 {
 		panic("odd number of src and dst")
 	}
-	return func(e *Ex) bool {
+	return func(e Ex) bool {
 		for i := 0; i < len(srcdst); i = i + 2 {
-			if e.Src == srcdst[i] && e.Dst == srcdst[i+1] {
+			if e.Src() == srcdst[i] && e.Dst() == srcdst[i+1] {
 				return true
 			}
 		}
@@ -93,9 +103,9 @@ func WithSrcDst(srcdst ...string) ExFilter {
 }
 
 func WithGroup(groups ...string) ExFilter {
-	return func(e *Ex) bool {
+	return func(e Ex) bool {
 		for i := range groups {
-			if e.Group == groups[i] {
+			if e.Group() == groups[i] {
 				return true
 			}
 		}
