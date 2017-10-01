5cf4e55d2b4624e014299d3beea0183e98b4df2b
Dima Kurguzov <koorgoo@gmail.com>
2017-10-01 11:08:08 +0300

    Add server with prometheus metrics

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/koorgoo/vtb24/api"
 	"github.com/koorgoo/vtb24/bank"
 	"github.com/koorgoo/vtb24/config"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var DefaultFilters = []bank.ExFilter{
@@ -33,7 +38,16 @@ func main() {
 		log.Fatal(err)
 	}
 
-	_ = cfg
+	termc := make(chan os.Signal)
+	signal.Notify(termc, os.Interrupt, syscall.SIGTERM)
+
+	servec := make(chan error)
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		if err := http.ListenAndServe(cfg.WebAddr, nil); err != nil {
+			servec <- err
+		}
+	}()
 
 	c := new(api.Client)
 	resp, err := c.Request()
@@ -46,5 +60,11 @@ func main() {
 
 	for _, e := range ex {
 		fmt.Println(e)
+	}
+
+	select {
+	case <-termc:
+	case err := <-servec:
+		log.Fatal(err)
 	}
 }
