b95554826e50877417fe3c273c06f99dc1e35a3d
Dima Kurguzov <koorgoo@gmail.com>
2017-10-02 11:26:30 +0300

    Add Config.RatesTimeout

--- config/config.go
+++ config/config.go
@@ -5,11 +5,15 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"time"
 )
+
+const DefaultRatesTimeout = 5 * time.Minute
 
 type Config struct {
 	WebAddr       string        `json:"web_addr"`
 	TelegramToken string        `json:"telegram_token"`
+	RatesTimeout  time.Duration `json:"rates_timeout"`
 	Donate        *DonateConfig `json:"donate"`
 }
 
@@ -22,6 +26,12 @@ var (
 	errWebAddr       = errors.New("invalid web addr")
 	errTelegramToken = errors.New("invalid telegram token")
 )
+
+func (c *Config) setDefaults() {
+	if c.RatesTimeout == 0 {
+		c.RatesTimeout = DefaultRatesTimeout
+	}
+}
 
 func (c *Config) validate() error {
 	if c.WebAddr == "" {
@@ -44,6 +54,7 @@ func ParseJSON(filename string) (c Config, err error) {
 		err = fmt.Errorf("config: %s: %s", filename, err)
 		return
 	}
+	c.setDefaults()
 	if err = c.validate(); err != nil {
 		err = fmt.Errorf("config: validation: %s", err)
 		return
