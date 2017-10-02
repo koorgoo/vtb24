da3f963344b5eb6e75bef1f3023af552161577a1
Dima Kurguzov <koorgoo@gmail.com>
2017-10-02 06:42:52 +0300

    Reply with rates in telegram chat

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 
+	"github.com/koorgoo/telegram"
 	"github.com/koorgoo/vtb24/api"
 	"github.com/koorgoo/vtb24/bank"
+	"github.com/koorgoo/vtb24/chat"
 	"github.com/koorgoo/vtb24/config"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
@@ -41,30 +45,85 @@ func main() {
 	termc := make(chan os.Signal)
 	signal.Notify(termc, os.Interrupt, syscall.SIGTERM)
 
-	servec := make(chan error)
+	errc := make(chan error, 1)
+	ex, err := GetDefaultEx()
+	if err != nil {
+		errc <- err
+	}
+
 	go func() {
 		http.Handle("/metrics", promhttp.Handler())
 		if err := http.ListenAndServe(cfg.WebAddr, nil); err != nil {
-			servec <- err
+			errc <- err
 		}
 	}()
 
-	c := new(api.Client)
-	resp, err := c.Request()
-	if err != nil {
-		log.Fatal(err)
-	}
+	go func() {
+		bot, err := telegram.NewBot(context.TODO(), cfg.TelegramToken)
+		if err != nil {
+			errc <- err
+			return
+		}
 
-	ex := bank.ParseEx(resp)
-	ex = bank.FilterEx(ex, DefaultFilters...)
+		go func(updatec <-chan *telegram.Update) {
+			for update := range updatec {
+				if update.Message == nil {
+					continue
+				}
+				if update.Message.Text == nil {
+					continue
+				}
 
-	for _, e := range ex {
-		fmt.Println(e)
-	}
+				n, err := strconv.ParseFloat(*update.Message.Text, 64)
+				if err != nil {
+					_, _ = bot.SendMessage(context.TODO(), &telegram.TextMessage{
+						ChatID: update.Message.Chat.ID,
+						Text:   "Я понимаю только числа.",
+					})
+					continue
+				}
+
+				text, mode, err := chat.MakeMessage(n, ex)
+				if err != nil {
+					_, _ = bot.SendMessage(context.TODO(), &telegram.TextMessage{
+						ChatID: update.Message.Chat.ID,
+						Text:   fmt.Sprintf("Не удалось обменять %v.", n),
+					})
+					continue
+				}
+
+				_, err = bot.SendMessage(context.TODO(), &telegram.TextMessage{
+					ChatID:    update.Message.Chat.ID,
+					Text:      text,
+					ParseMode: mode,
+				})
+				if err != nil {
+					log.Println(err)
+				}
+			}
+		}(bot.Updates())
+
+		go func(errorc <-chan error) {
+			for err := range errorc {
+				log.Println(err)
+			}
+		}(bot.Errors())
+	}()
 
 	select {
 	case <-termc:
-	case err := <-servec:
+	case err := <-errc:
 		log.Fatal(err)
 	}
+}
+
+func GetDefaultEx() ([]bank.Ex, error) {
+	c := new(api.Client)
+	resp, err := c.Request()
+	if err != nil {
+		return nil, err
+	}
+	ex := bank.ParseEx(resp)
+	ex = bank.FilterEx(ex, DefaultFilters...)
+	return ex, nil
 }
